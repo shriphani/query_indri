424451e81430cd2555cc64f8f29b08b2f92a20ee
Shriphani Palakodety <shriphanip@gmail.com>
2014-07-11 15:40:05 -0400

    sample for how to query docs

--- query_indri.cpp
+++ query_indri.cpp
@@ -1,9 +1,22 @@
 #include <vector>
 #include "indri/QueryEnvironment.hpp"
 
+using namespace indri::api;
+
 int main() {
 
-  cout << "Hello" << endl;
+  QueryEnvironment indriEnvironment;
 
-  return 0;
+  indriEnvironment.addServer("10.1.1.246:9001");
+
+  std::vector<ScoredExtentResult> results = indriEnvironment.runQuery("#1(barack obama)", 5);
+
+  std::vector<int> docIds;
+
+  std::vector<std::string> urls = indriEnvironment.documentMetadata(results, "url");
+
+  for (std::vector<std::string>::iterator it = urls.begin(); it != urls.end(); ++it) {
+    cout << *it << endl;
+  }
+
 }
